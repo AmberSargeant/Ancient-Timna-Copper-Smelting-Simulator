53af44a9391641308a93ac4aac33bc8d20dd02d1
Amber Sargeant <amber.sargeant@gmail.com>
2021-10-14 13:01:31 -0700

    Arduino now works with Unity

---
+++ Ancient-Timna-Copper-Smelting-Simulator/Assets/Scripts/MessageListener.cs
@@ -0,0 +1,51 @@
+﻿/**
+ * Ardity (Serial Communication for Arduino + Unity)
+ * Author: Daniel Wilches <dwilches@gmail.com>
+ *
+ * This work is released under the Creative Commons Attributions license.
+ * https://creativecommons.org/licenses/by/2.0/
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * When creating your message listeners you need to implement these two methods:
+ *  - OnMessageArrived
+ *  - OnConnectionEvent
+ */
+
+public class MessageListener : MonoBehaviour
+{
+    int temperature;
+    // Invoked when a line of data is received from the serial device.
+    public void OnMessageArrived(string msg)
+    {
+        string[] msgSplit = msg.Split(' ');
+        temperature = int.Parse(msgSplit[0]);
+    }
+
+    // Invoked when a connect/disconnect event occurs. The parameter 'success'
+    // will be 'true' upon connection, and 'false' upon disconnection or
+    // failure to connect.
+
+    void OnConnectionEvent(bool success)
+    {
+        if (success)
+            Debug.Log("Connection established");
+        else
+            Debug.Log("Connection attempt failed or disconnection detected");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(temperature);
+    }
+}
