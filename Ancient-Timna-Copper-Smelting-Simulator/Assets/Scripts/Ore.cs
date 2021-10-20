c781bb2aeba0cb3b2ee276bb4ac45953e2cdb697
Amber Sargeant <amber.sargeant@gmail.com>
2021-10-20 16:48:26 -0700

    Removed new unity input system

--- Ancient-Timna-Copper-Smelting-Simulator/Assets/Scripts/Ore.cs
+++ Ancient-Timna-Copper-Smelting-Simulator/Assets/Scripts/Ore.cs
@@ -27,10 +27,6 @@ public class Ore : MonoBehaviour
         Debug.Log(OVRInput.GetConnectedControllers());
         OVRInput.Update();
         OVRInput.FixedUpdate();
-        if (OVRInput.Get(OVRInput.Button.One))
-        {
-            print("Voila!");
-        }
     }
 
 
