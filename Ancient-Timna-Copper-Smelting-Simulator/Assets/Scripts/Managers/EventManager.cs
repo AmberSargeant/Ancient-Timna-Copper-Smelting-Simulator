bae1fe83f5a314a7023a4c22862819ca227ba592
Amber Sargeant <amber.sargeant@gmail.com>
2021-10-28 14:06:09 -0700

    Achieved player choose ore functionality

--- Ancient-Timna-Copper-Smelting-Simulator/Assets/Scripts/Managers/EventManager.cs
+++ Ancient-Timna-Copper-Smelting-Simulator/Assets/Scripts/Managers/EventManager.cs
@@ -1,18 +1,137 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class EventManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private PlayerCollision playerCollision;
+    private VesselCollision vesselCollision;
+    private bool firstStage = true;
+    private bool secondStage,thirdStage, fourthStage = false;
+    public List<GameObject> ore = new List<GameObject>();
+    public GameObject largeOrePrefab;
+    public GameObject largeOreTable;
+    public GameObject smallOrePrefab;
+    public GameObject smallOreTable;
+    public GameObject largeOreText;
+    public GameObject smallOreText;
+    public GameObject firstStagetext;
+    public GameObject secondStagetext;
+    public GameObject thirdStagetext;
     void Start()
     {
-        
+        playerCollision = FindObjectOfType<PlayerCollision>();
+        vesselCollision = FindObjectOfType<VesselCollision>();
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        //need to refactor
+        if (firstStage)
+        {
+            if (playerCollision.smallOre)
+            {
+                //need to refactor
+                smallOreText.SetActive(true);
+            }
+            else
+            {
+                //need to refactor
+                smallOreText.SetActive(false);
+            }
+
+            if (playerCollision.largeOre)
+            {
+                //need to refactor
+                largeOreText.SetActive(true);
+            }
+            else
+            {
+                //need to refactor
+                largeOreText.SetActive(false);
+            }
+
+            if (OVRInput.GetDown(OVRInput.Button.One))
+            {
+                firstStagetext.SetActive(false);
+                smallOrePrefab.SetActive(false);
+                smallOreTable.SetActive(false);
+                //need to refactor
+                firstStage = false;
+                secondStage = true;
+            }
+            else if (OVRInput.GetDown(OVRInput.Button.Two))
+            {
+                firstStagetext.SetActive(false);
+                largeOrePrefab.SetActive(false);
+                largeOreTable.SetActive(false);
+                //need to refactor
+                firstStage = false;
+                secondStage = true;
+            }
+        }
+
+        //need to refactor
+        if (secondStage)
+        {
+            //need to refactor
+            secondStagetext.SetActive(true);
+            largeOreText.SetActive(false);
+            smallOreText.SetActive(false);
+            //need to refactor
+            if (vesselCollision.bigOre1)
+            {
+                ore[0].SetActive(false);
+            }
+            if (vesselCollision.bigOre2)
+            {
+                ore[1].SetActive(false);
+            }
+            if (vesselCollision.bigOre3)
+            {
+                ore[2].SetActive(false);
+            }
+            if (vesselCollision.bigOre4)
+            {
+                ore[3].SetActive(false);
+            }
+            if (vesselCollision.bigOre5)
+            {
+                ore[4].SetActive(false);
+            }
+            if (vesselCollision.smallOre1)
+            {
+                ore[5].SetActive(false);
+            }
+            if (vesselCollision.smallOre2)
+            {
+                ore[6].SetActive(false);
+            }
+            if (vesselCollision.smallOre3)
+            {
+                ore[7].SetActive(false);
+            }
+            if (vesselCollision.smallOre4)
+            {
+                ore[8].SetActive(false);
+            }
+            if (vesselCollision.smallOre5)
+            {
+                ore[9].SetActive(false);
+            }
+            if(vesselCollision.countOre == 5)
+            {
+                //need to refactor
+                secondStage = false;
+                thirdStage = true;
+            }
+
+        }
+
+        //need to refactor
+        if (thirdStage)
+        {
+            thirdStagetext.SetActive(true);
+        }
     }
 }
---
+++ Ancient-Timna-Copper-Smelting-Simulator/Assets/Scripts/Objects/VesselCollision.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VesselCollision : MonoBehaviour
+{
+    public bool bigOre1, bigOre2, bigOre3, bigOre4, bigOre5 = false;
+    public bool smallOre1, smallOre2, smallOre3, smallOre4, smallOre5 = false;
+    public int countOre;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        //Check for a match with the specified name on any GameObject that collides with your GameObject
+        if (collision.gameObject.name == "Big Ore 1")
+        {
+            bigOre1 = true;
+            countOre++;
+        }
+
+        if (collision.gameObject.name == "Big Ore 2")
+        {
+            bigOre2 = true;
+            countOre++;
+        }
+
+        if (collision.gameObject.name == "Big Ore 3")
+        {
+            bigOre3 = true;
+            countOre++;
+        }
+
+        if (collision.gameObject.name == "Big Ore 4")
+        {
+            bigOre4 = true;
+            countOre++;
+        }
+
+        if (collision.gameObject.name == "Big Ore 5")
+        {
+            bigOre5 = true;
+            countOre++;
+        }
+
+        if (collision.gameObject.name == "Small Ore 1")
+        {
+            smallOre1 = true;
+            countOre++;
+        }
+
+        if (collision.gameObject.name == "Small Ore 2")
+        {
+            smallOre2 = true;
+            countOre++;
+        }
+
+        if (collision.gameObject.name == "Small Ore 3")
+        {
+            smallOre3 = true;
+            countOre++;
+        }
+
+        if (collision.gameObject.name == "Small Ore 4")
+        {
+            smallOre4 = true;
+            countOre++;
+        }
+
+        if (collision.gameObject.name == "Small Ore 5")
+        {
+            smallOre5 = true;
+            countOre++;
+        }
+
+
+    }
+}
