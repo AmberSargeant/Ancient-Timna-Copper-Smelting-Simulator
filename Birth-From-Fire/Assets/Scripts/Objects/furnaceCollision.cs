9cc956bab5969da45798da5a252bdf18a00cb601
Amber Sargeant <amber.sargeant@gmail.com>
2021-11-07 18:00:05 -0800

    Redid Project Files

---
+++ Birth-From-Fire/Assets/Scripts/Objects/furnaceCollision.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FurnaceCollision : MonoBehaviour
+{
+    public bool inFurnace;
+    public bool pipeInFurnace;
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.tag == "Vessel")
+        {
+            inFurnace = true;
+        }
+
+        if (other.tag == "BlowPipe")
+        {
+            pipeInFurnace = true;
+        }
+    }
+}
