cc8a6ad45bfc8ba26b4fe3d9baf3cc2e21bba650
Amber Sargeant <amber.sargeant@gmail.com>
2021-11-16 19:25:55 -0800

    Attempted to fix xr ray interactor issue

---
+++ Birth-From-Fire/Assets/Scripts/Objects/ToggleGravity.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleGravity : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void enableGravity() {
+        this.GetComponent<Rigidbody>().isKinematic = false;
+        this.GetComponent<Rigidbody>().useGravity = true;
+    }
+
+    //public void disableGravity()
+    //{
+    //    this.GetComponent<Rigidbody>().isKinematic = true;
+    //    this.GetComponent<Rigidbody>().useGravity = false;
+    //}
+}
