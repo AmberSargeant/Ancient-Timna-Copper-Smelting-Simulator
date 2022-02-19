2481f3efd3264b64200d95cb6115f0a6d221fdf1
Amber Sargeant <amber.sargeant@gmail.com>
2022-02-18 16:06:57 -0800

    Changed text so now its within the scene

---
+++ Birth-From-Fire/Assets/Scripts/XR/OffsetGrab.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class OffsetGrab : XRGrabInteractable
+{
+    [System.Obsolete]
+    protected override void OnSelectEntering(XRBaseInteractor interactor)
+    {
+
+    }
+}
